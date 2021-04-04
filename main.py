324a2a9a31c59df4257ba8d5730b03aa27e0e9a2
Marc <mb800915@ohio.edu>
2021-04-04 12:51:38 -0400

    added error handling

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.clock import Clock
+from kivy.uix.popup import Popup
 
 kivy.require("1.10.1")
 Config.set('graphics', 'resizable', True)
@@ -32,28 +33,64 @@ class RLPathButton(Button):
     @staticmethod
     def get_path():
         global rl_path
+        old_path = rl_path
         root = tk.Tk()
         root.withdraw()
-        rl_path = filedialog.askdirectory()
-        f = open("rl_path.txt", "w")
+        rl_path = filedialog.askdirectory(title="Select rocketleague folder")
+        if rl_path == '':
+            rl_path = old_path
+        os.chdir(home_dir)
+        f = open("rl_path.txt", "w+")
         f.write(rl_path)
-        f.close
+        f.close()
         return rl_path
 
 class MapPathButton(Button):
     @staticmethod
     def get_path():
         global map_path
+        old_path = map_path
         root = tk.Tk()
         root.withdraw()
-        map_path = filedialog.askdirectory()
-        f = open("map_path.txt", "w")
+        map_path = filedialog.askdirectory(title="Select Custom Maps folder")
+        if map_path == '':
+            map_path = old_path
+        os.chdir(home_dir)
+        f = open("map_path.txt", "w+")
         f.write(map_path)
-        f.close
+        f.close()
         return map_path
 
 class MapLoader(Widget):
+
+    def error(self, reason):
+        content = Button(text="Close")
+        popup = Popup(title="Error: "+reason,
+                content=content,
+                size_hint=(None, None), size=(500, 100),
+                auto_dismiss=False)
+
+        content.bind(on_press=popup.dismiss)
+
+        popup.open()
+
     def reset(self):
+        # check for paths not specified
+        if map_path == '' and rl_path == '':
+            reason = "Map folder path and Rocket League folder path not specified"
+            self.error(reason)
+            return
+
+        if map_path == '':
+            reason = "Map folder path not specified"
+            self.error(reason)
+            return
+        
+        if rl_path == '':
+            reason = "Rocket League folder path not specified"
+            self.error(reason)
+            return
+
         # reset all button colors
         buttons = self.children[0].children[0].children
         for b in buttons:
@@ -70,10 +107,19 @@ class MapLoader(Widget):
                     os.remove(full_rl_path+"\\Labs_Underpass_P.upk")
 
         # put original bak from this repo
-        copy2(home_dir+"\\Labs_Underpass_P.upk", full_rl_path)          
+        copy2(home_dir+"\\Labs_Underpass_P.upk", full_rl_path)
+
+        # tell user
+        content = Button(text="Close")
+        popup = Popup(title="Game files successfully reset",
+                content=content,
+                size_hint=(None, None), size=(300, 100),
+                auto_dismiss=False)
+
+        content.bind(on_press=popup.dismiss)
+        popup.open()          
     
     def load_maps(self, *args):
-        print(map_path)
         if map_path != '':
             # remove previous maps
             for c in list(self.children):
@@ -88,7 +134,7 @@ class MapLoader(Widget):
             grid = GridLayout(pos=(self.x+50, self.height-300), 
                             size=(200, 200), 
                             rows=math.ceil(dir_count/3), cols=3,
-                            col_default_width=250, row_default_height=250,
+                            col_default_width=200, row_default_height=200,
                             spacing=(30, 30), size_hint_y=None, padding=(30, 30))   
             grid.bind(minimum_height=grid.setter('height'))
                    
@@ -96,14 +142,7 @@ class MapLoader(Widget):
             for i in range(dir_count):
                 os.chdir(path+"/"+dirs[i])
 
-                # get image file
-                pic = glob.glob("./*.png")
-                if len(pic) == 0:
-                    pic = glob.glob("./*.jpg")[0]
-                else:
-                    pic = pic[0]
-
-                # create button and imagev for it
+                # create button
                 btn = Button(text=dirs[i])
                 btn.bind(on_press=self.select_map)
                 grid.add_widget(btn)
@@ -118,15 +157,17 @@ class MapLoader(Widget):
     def update_RLPATH(self):
         self.ids["RLPATH"].text = "RL Path: " + rl_path
 
-    def update_RLPATH_save(self, *args):
-        self.ids["RLPATH"].text = "RL Path: " + rl_path
-        Clock.unschedule(self.update_RLPATH_save)
-
     def update_MAPPATH(self):
         self.ids["MAPPATH"].text = "Map Folder Path: " + map_path
         self.load_maps()
     
     def select_map(self, instance):
+        # check if rl path is specified
+        if rl_path == '':
+            reason = "Rocket League folder path not specified"
+            self.error(reason)
+            return
+
         # remove the green button
         buttons = self.children[0].children[0].children
         for b in buttons:
@@ -150,7 +191,13 @@ class MapLoader(Widget):
         # copy selected map to directory
         full_map_path = map_path+"/"+instance.text
         os.chdir(full_map_path)
-        udk = glob.glob("*.udk")[0]
+        udk = glob.glob("*.udk")
+        if len(udk) == 0:
+            instance.background_color=(1, 1, 1, 1)
+            reason = "udk file not found"
+            self.error(reason)
+            return
+        udk = udk[0]    
         full_map_path = full_map_path+"/"+udk
         copy2(full_map_path, full_rl_path+"/Labs_Underpass_P.upk")
        
@@ -162,28 +209,20 @@ class MapLoaderApp(App):
     load_save = False
 
     os.chdir("./")
-    found1 = False
     if os.path.exists("rl_path.txt"):
         f = open("rl_path.txt", "r")
         rl_path = f.readlines()[0]
         rl_path_save = "RL Path: " + rl_path
-        found1 = True
-        f.close()
     else:
         rl_path_save = "RL Path: "
 
-    found2 = False
     if os.path.exists("map_path.txt"):
         f = open("map_path.txt", "r")
         map_path = f.readlines()[0]
         map_path_save = "Map Folder Path: " + map_path
-        found2 = True
-        f.close()
+        load_save = True
     else:
         map_path_save = "Map Folder Path: "
-
-    if found1 and found2:
-        load_save = True
 
     def update(self, *args):
         pass
